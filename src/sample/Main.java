e194e002546b1a225c09fba03dbc56006aa24296
Shhatter <bartez378@gmail.com>
2017-06-11 16:59:36 +0200

    Remind of binding controller to button and some method functionality
    Added simple connection to a server-

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,16 +8,11 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        /*
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));*//*
-*//*
-primaryStage.setTitle("Hello World");
-*//*
-        primaryStage.setScene(new Scene(root, 600, 800));
 
-        primaryStage.show();*/
 
         FXMLLoader mainControllerLoader = new FXMLLoader();
         mainControllerLoader.setLocation(getClass().getResource("/sample/sample.fxml"));
---
+++ src/sample/PawnColor.java
@@ -0,0 +1,9 @@
+package sample;
+
+/**
+ * Created by Praca on 2017-06-11.
+ */
+public enum PawnColor {
+
+    BLACK,WHITE,NONE
+}
